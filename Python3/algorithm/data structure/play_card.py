24c10f4e5d30915eae47bd2af1382803fe4961e4
hugh225 <pentium_3@homtail.com>
2017-03-28 16:45:35 +0800

    play card python

--- Python3/algorithm/data structure/play_card.py
+++ Python3/algorithm/data structure/play_card.py
@@ -1,34 +1,43 @@
-# 发牌
-class Queue(object):
-	"""docstring for Queue"""
-	def __init__(self):
-		self.items=[]
-
-	def isEmpety(self):
-		return self.items==[]
-
-	def size(self):
-		return len(self.items)
-
-	def enqueue(self, item):
-		self.items.insert(0,item)
-
-	def dequeue(self, item):
-		self.items.pop()
-
+s=[]
+b=[0 for x in range(0,10)]
 # 发牌
 while True:
-	a=[int(n) for n in input().split(',')]
-	b=[int(n) for n in input().split(',')]
-	if len(a)==len(b):
+	player_a=[int(n) for n in input().split(',')]
+	player_b=[int(n) for n in input().split(',')]
+	if len(player_a)==len(player_b):
 		break
 	print("cards not equal, please try again!")
 
 # 打牌
-def play_card(player):
-	t=player[-1]
+def play_card(player, table, book):
+	t=player[0]
+	if(book[t]==0):
+		player.pop(0)
+		table.append(t)
+		book[t]=1
+	else:
+		player.pop(0)
+		player.append(t)
+		while table[-1]!=t:
+			x=table.pop()
+			player.append(x)
+			book[x]=0
+		player.append(table.pop())
+		book[t]=0
 
+while len(player_a)>0 and len(player_b)>0:
+	play_card(player_a, s, b)
+	play_card(player_b, s, b)
 
-while True:
-	
-
+if len(player_a)==0:
+	print("player b wins!")
+	print("the cards on player b:")
+	print(player_b)
+	print("the cards on table:")
+	print(s)
+elif len(player_b)==0:
+	print("player a wins!")
+	print("the cards on player a:")
+	print(player_a)
+	print("the cards on table:")
+	print(s)
